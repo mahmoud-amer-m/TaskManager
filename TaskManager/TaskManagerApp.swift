53990dedda8714e24b612e2438983433b670af75
Mahmoud Amer <mahmoud.amer.m@gmail.com>
2026-03-27 11:25:04 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Mahmoud Amer on 27.03.26.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
